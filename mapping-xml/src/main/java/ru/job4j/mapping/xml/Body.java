313c6a0eb17281c3db4e096cd68523bce060920a
Andrei Soloviev <hedg.r52@gmail.com>
2019-09-04 22:47:05 +0300

    Реализовать модели и связи для задание SQL хранилище машин.

---
+++ mapping-xml/src/main/java/ru/job4j/mapping/xml/Body.java
@@ -0,0 +1,46 @@
+package ru.job4j.mapping.xml;
+
+import java.util.Objects;
+
+public class Body {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Body() {
+    }
+
+    public Body(long id) {
+        this.id = id;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Body body = (Body) o;
+        return id == body.id && Objects.equals(name, body.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
